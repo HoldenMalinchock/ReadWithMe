24437a5003fb0567e10526dd8f342e42ef0f4d3b
Holden <h.malinchock@verizon.net>
2020-01-31 15:06:42 -0500

    Established some routing structure.  Looking to get more basic routing to work shortly

--- web-app/src/main.js
+++ web-app/src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from "vuetify"
 import vuetify from './plugins/vuetify';
 
-Vue.use(Vuetify)
+Vue.use(vuetify)
 Vue.config.productionTip = false
 
 new Vue({
