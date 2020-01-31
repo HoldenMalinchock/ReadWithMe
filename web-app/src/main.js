403e7043610f71f77d9011534640f6e981988b6e
Holden <h.malinchock@verizon.net>
2020-01-31 14:36:42 -0500

    Created Vuetify webapp structure, added git ignore

---
+++ web-app/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from "vuetify"
+import vuetify from './plugins/vuetify';
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
