ee10f022b4857493d9b2f1963d2639c5db5c6bc9
Holden Malinchock <h.malinchock@verizon.net>
2020-04-28 23:16:36 -0400

    Images of where the people will be displayed is looking better. Images have a hover feature on them to distinguish them as buttons. Made the images clickable

--- web-app/src/components/Video.vue
+++ web-app/src/components/Video.vue
@@ -17,13 +17,67 @@
     <v-flex xs12 sm6 offset-sm3>
         <v-toolbar flat>
         </v-toolbar>
-        <v-container fluid grid-list-sm>
-          <v-layout row wrap>
-            <v-flex v-for="i in 6" :key="i" xs4>
-              <img :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" class="image" alt="lorem" width="100%" height="100%">
-            </v-flex>
-          </v-layout>
-        </v-container>
+        <v-container class="pa-4 text-center">
+    <v-row class="fill-height" align="center" justify="center">
+      <template v-for="name in names">
+        <v-col
+          :key="name"
+          cols="12"
+          md="4"
+          @click="go_to_video()"
+        >
+          <v-hover v-slot:default="{ hover }">
+            <v-card
+              :elevation="hover ? 12 : 2"
+              :class="{ 'on-hover': hover }"
+            >
+              <v-img
+                :src="`https://rwm-videos.s3.amazonaws.com/${book}/${name}/picture/${name}.jpg`"
+                height="225px"
+              >
+                <v-card-title class="title white--text">
+                  <v-row
+                    class="fill-height flex-column"
+                    justify="space-between"
+                  >
+                  <!--
+                    <p class="mt-4 subheading text-left">{{ item.title }}</p>
+
+                    <div>
+                      <p class="ma-0 body-1 font-weight-bold font-italic text-left">
+                        {{ item.text }}
+                      </p>
+                      <p class="caption font-weight-medium font-italic text-left">
+                        {{ item.subtext }}
+                      </p>
+                    </div>
+                    
+                    <div class="align-self-center">
+                      <v-btn
+                        v-for="(icon, index) in icons"
+                        :key="index"
+                        :class="{ 'show-btns': hover }"
+                        color="transparent"
+                        icon
+                      >
+                        <v-icon
+                          :class="{ 'show-btns': hover }"
+                          color="transparent"
+                        >
+                          {{ icon }}
+                        </v-icon>
+                      </v-btn>
+                    </div>
+                    -->
+                  </v-row>
+                </v-card-title>
+              </v-img>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
     </v-flex>
   </v-layout>
 
@@ -47,7 +101,14 @@
     data: () => ({
       drawer: null,
       data: null,
-      sent: false
+      sent: false,
+      hover: false,
+      names: [
+        'Bill Mines',
+        'Holden Malinchock',
+        'John Long'
+      ],
+      book: '69e7a3bfd950f20775821027c976f398'
     }),
     methods: {
         go_to_login(){
@@ -57,6 +118,9 @@
         },
         go_to_home(){
           this.$router.push("/").catch(err => {err})
+        },
+        go_to_video(){
+          console.log('GO TO VIDEO!')
         }
     }
   }
