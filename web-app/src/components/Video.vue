12c91fe72a9ac8098825314bafc42d5d3da13f2a
Holden Malinchock <h.malinchock@verizon.net>
2020-04-29 00:02:06 -0400

    Updated to formatting with names

--- web-app/src/components/Video.vue
+++ web-app/src/components/Video.vue
@@ -13,17 +13,13 @@
       <!-- This is an interesting feature I may want to use later <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
     </v-app-bar>
 
-    <v-layout row>
-    <v-flex xs12 sm6 offset-sm3>
-        <v-toolbar flat>
-        </v-toolbar>
         <v-container class="pa-4 text-center">
     <v-row class="fill-height" align="center" justify="center">
       <template v-for="name in names">
         <v-col
           :key="name"
           cols="12"
-          md="4"
+          md="2"
           @click="go_to_video()"
         >
           <v-hover v-slot:default="{ hover }">
@@ -40,46 +36,16 @@
                     class="fill-height flex-column"
                     justify="space-between"
                   >
-                  <!--
-                    <p class="mt-4 subheading text-left">{{ item.title }}</p>
-
-                    <div>
-                      <p class="ma-0 body-1 font-weight-bold font-italic text-left">
-                        {{ item.text }}
-                      </p>
-                      <p class="caption font-weight-medium font-italic text-left">
-                        {{ item.subtext }}
-                      </p>
-                    </div>
-                    
-                    <div class="align-self-center">
-                      <v-btn
-                        v-for="(icon, index) in icons"
-                        :key="index"
-                        :class="{ 'show-btns': hover }"
-                        color="transparent"
-                        icon
-                      >
-                        <v-icon
-                          :class="{ 'show-btns': hover }"
-                          color="transparent"
-                        >
-                          {{ icon }}
-                        </v-icon>
-                      </v-btn>
-                    </div>
-                    -->
                   </v-row>
                 </v-card-title>
               </v-img>
             </v-card>
           </v-hover>
+          <p>{{name}}</p>
         </v-col>
       </template>
     </v-row>
   </v-container>
-    </v-flex>
-  </v-layout>
 
     <v-footer
       color="primary"
